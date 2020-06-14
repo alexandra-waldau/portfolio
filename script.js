92f91bac54a6ca3e01ac1c96e3990f3f1ce047d1
Alexandra Waldau <alew@itu.dk>
2020-06-14 22:40:47 +0200

    added career section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Navigation
+
 let navRight = document.getElementById('js-menu-1');
 let navLeft = document.getElementById('js-menu-2');
 
@@ -8,3 +10,21 @@ navToggle.addEventListener('click', function () {
     navRight.classList.toggle('active');
     navLeft.classList.toggle('active');
 });
+
+//Swiping
+
+let swipeRight = document.querySelector('#swipe-right');
+let swipeLeft = document.querySelector('#swipe-left');
+
+swipeRight.addEventListener('click', function() {
+    $('.headline.swipe.right').hide();
+    $('.headline.swipe.left').show();
+    //substitute display: block with display: flex
+    $('.headline.swipe.left').css('display', 'flex');
+});
+
+swipeLeft.addEventListener('click', function() {
+    $('.headline.swipe.right').show();
+    $('.headline.swipe.left').hide();
+});
+
