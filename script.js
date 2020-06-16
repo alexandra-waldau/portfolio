b2da9c452053b3a9ca71920229ab7f5c8a033c08
Alexandra Waldau <alew@itu.dk>
2020-06-16 22:14:43 +0200

    added mobile career section

--- script.js
+++ script.js
@@ -15,16 +15,22 @@ navToggle.addEventListener('click', function () {
 
 let swipeRight = document.querySelector('#swipe-right');
 let swipeLeft = document.querySelector('#swipe-left');
+let timeLineEd = document.querySelector('.timeline.education');
+let timeLineWork = document.querySelector('.timeline.work-experience');
 
 swipeRight.addEventListener('click', function() {
     $('.headline.swipe.right').hide();
     $('.headline.swipe.left').show();
     //substitute display: block with display: flex
     $('.headline.swipe.left').css('display', 'flex');
+    $('.timeline.work-experience').hide();
+    $('.timeline.education').show();
 });
 
 swipeLeft.addEventListener('click', function() {
     $('.headline.swipe.right').show();
     $('.headline.swipe.left').hide();
+    $('.timeline.education').hide();
+    $('.timeline.work-experience').show();
 });
 
